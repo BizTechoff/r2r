11aac4132344bbea90e06e7ae5faf1911fbfe385
BizTechoff <biztechoff@gmail.com>
2021-04-11 11:50:42 +0300

    demo enm done

--- src/app/demo/demo-enum/demo-enum.component.ts
+++ src/app/demo/demo-enum/demo-enum.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataAreaSettings, StringColumn } from '@remult/core';
+import { DataAreaSettings, StringColumn, ValueListColumn } from '@remult/core';
 
 @Component({
   selector: 'app-demo-enum',
@@ -7,13 +7,39 @@ import { DataAreaSettings, StringColumn } from '@remult/core';
   styleUrls: ['./demo-enum.component.scss']
 })
 export class DemoEnumComponent implements OnInit {
-  a = new StringColumn();
+  a = new ValueListColumn(Status);
   area = new DataAreaSettings({
-    columnSettings:()=>[this.a]
+    columnSettings: () => [this.a]
   });
   constructor() { }
 
   ngOnInit() {
   }
+  click(){
+    
+    if (this.a.value==Status.open){
+      alert('פתוח');
+    }
+  }
 
+}
+export class AnotherEnum{
+  static kukiriku = new AnotherEnum('val1');
+  static val2 = new AnotherEnum();
+  constructor(public id?:string){
+
+  }
+}
+
+export class Status {
+  static open = new Status(0, 'פתוח',);
+  static closed = new Status(1, 'סגור','red');
+  constructor(public id: number, public caption: string,public color='green') {
+
+  }
+}
+export class StatusColumn extends ValueListColumn<Status>{
+  constructor() {
+    super(Status);
+  }
 }
